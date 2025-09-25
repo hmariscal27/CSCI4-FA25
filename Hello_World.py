362e7b49acc5b5e31a78d02a7addb2026643888a
Hector Mariscal Jr <hmariscal002@student.butte.edu>
2025-09-25 09:29:12 -0700

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+print("Hello, World!")
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
